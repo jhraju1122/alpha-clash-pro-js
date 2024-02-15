412a73efc73e37433180aad4b6c1350189abbc7a
jhraju <jhraju890@gmail.com>
2024-02-15 11:42:32 +0600

    added playGround and js

---
+++ scripts/alfa-clash.js
@@ -0,0 +1,9 @@
+//  function play(){
+//    //step-1: hide the home screen.to hide the screen add the class hidden to the home section .
+//    const homeSection  = document.getElementById('home-screen');
+//    homeSection.classList.add('hidden')
+// //    console.log(homeSection.classList);
+//    //show the playGround
+//    const playGround =document.getElementById('Play-Ground');
+//    playGround.classList.remove('hidden');
+//  }
