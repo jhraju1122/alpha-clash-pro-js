0a50d6ddd1f8c8292e1196f2dd7b3351ec2356af
jhraju <jhraju890@gmail.com>
2024-02-15 17:56:20 +0600

    added showing random alphabet to the screen

--- scripts/alfa-clash.js
+++ scripts/alfa-clash.js
@@ -7,3 +7,38 @@
 //    const playGround =document.getElementById('Play-Ground');
 //    playGround.classList.remove('hidden');
 //  }
+
+
+function play(){
+  hideElementById('home-screen');
+  showElementById('Play-Ground');
+  const random = getARandomAlphabet();
+  document.getElementById('current-alphabet').innerText = random
+}
+
+//get or create an alphabet array.....
+function getARandomAlphabet(){
+  const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabets = alphabetString.split('');
+  // console.log(alphabets);
+
+//get a random index between 0 to 25
+const randomNumber = Math.random()*25;
+const index = Math.round(randomNumber);
+const alphabet = alphabets[index];
+// console.log(index, alphabet);
+return alphabet;
+}
+
+function continueGame(){
+  //step-1: generate a random alphabet
+  const alphabet = getARandomAlphabet();
+console.log(alphabet);
+  
+//set random generate alphabet to show screen
+const currentAlphabetElement = document.getElementById('current-alphabet');
+currentAlphabetElement.innerText = alphabet;
+}
+
+
+
